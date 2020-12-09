63e0e943ce8031bdde00206722078a89fdab47f1
JorritBosma <bosma.jorrit@gmail.com>
2020-12-09 19:19:00 +0100

    Added Authorization

--- routes/recensies.js
+++ routes/recensies.js
@@ -1,27 +1,16 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const { recensieSchema } = require('../schemas');
-
+const { validateRecensie, isIngelogd, isAuteurRecensie } = require('../middleware')
+const Wandeling = require('../models/wandeling');
+const Recensie = require('../models/recensie');
 const wrapAsync = require('../helpers/wrapAsync');
 const ExpressError = require('../helpers/ExpressError');
 
-const Wandeling = require('../models/wandeling');
-const Recensie = require('../models/recensie');
-
-const validateRecensie = (req, res, next) => {
-    const { error } = recensieSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-};
-
-router.post('/', validateRecensie, wrapAsync(async (req, res) => {
+router.post('/', isIngelogd, validateRecensie, wrapAsync(async (req, res) => {
     const { id } = req.params;
     const wandeling = await Wandeling.findById(id);
     const recensie = new Recensie(req.body.recensie);
+    recensie.auteur = req.user._id;
     wandeling.recensies.push(recensie);
     await recensie.save();
     await wandeling.save();
@@ -29,7 +18,7 @@ router.post('/', validateRecensie, wrapAsync(async (req, res) => {
     res.redirect(`/wandelingen/${wandeling._id}`)
 }));
 
-router.delete('/:recensieId', wrapAsync(async (req, res) => {
+router.delete('/:recensieId', isIngelogd, isAuteurRecensie, wrapAsync(async (req, res) => {
     const { id, recensieId } = req.params;
     await Wandeling.findByIdAndUpdate(id, { $pull: { recensies: recensieId } })
     await Recensie.findByIdAndDelete(recensieId);
