433f164a62c6fded66aef90052e5d26f77f3bb24
JorritBosma <bosma.jorrit@gmail.com>
2020-12-11 11:11:00 +0100

    Created my first controller! For Wandelingen. What a great trick to have learned.

--- routes/wandelingen.js
+++ routes/wandelingen.js
@@ -2,65 +2,21 @@ const express = require('express');
 const router = express.Router();
 const wrapAsync = require('../helpers/wrapAsync');
 const Wandeling = require('../models/wandeling');
+const wandelingen = require('../controllers/wandelingController');
 const { isIngelogd, validateWandeling, isAuteur } = require('../middleware');
 
+router.get('/', wrapAsync(wandelingen.index));
 
+router.get('/new', isIngelogd, wandelingen.renderNewForm);
 
-router.get('/', wrapAsync(async (req, res) => {
-    const wandelingen = await Wandeling.find({});
-    res.render('wandelingen/index', { wandelingen })
-}));
+router.post('/', isIngelogd, validateWandeling, wrapAsync(wandelingen.maakWandeling));
 
-router.get('/new', isIngelogd, (req, res) => {
-    res.render('wandelingen/new');
-});
+router.get('/:id', wrapAsync(wandelingen.toonWandeling));
 
-router.post('/', isIngelogd, validateWandeling, wrapAsync(async (req, res, next) => {
-    const wandeling = new Wandeling(req.body.wandeling);
-    wandeling.auteur = req.user._id;
-    await wandeling.save();
-    req.flash('success', 'Nieuwe wandeling aangemaakt!');
-    res.redirect(`wandelingen/${wandeling._id}`)
-}));
+router.get('/:id/edit', isIngelogd, isAuteur, wrapAsync(wandelingen.renderEditForm));
 
-router.get('/:id', wrapAsync(async (req, res) => {
-    const wandeling = await Wandeling.findById(req.params.id).populate({
-        path: 'recensies',
-        populate: {
-            path: 'auteur'
-        }
-    }).populate('auteur');
-    console.log(wandeling);
-    console.log(wandeling.auteur)
-    if (!wandeling) {
-        req.flash('error', 'Helaas, de wandeling is niet gevonden...');
-        return res.redirect('/wandelingen');
-    }
-    res.render('wandelingen/show', { wandeling });
-}));
+router.put('/:id', isIngelogd, isAuteur, validateWandeling, wrapAsync(wandelingen.wijzigWandeling));
 
-router.get('/:id/edit', isIngelogd, isAuteur, wrapAsync(async (req, res) => {
-    const { id } = req.params;
-    const wandeling = await Wandeling.findById(id);
-    if (!wandeling) {
-        req.flash('error', 'Helaas, de wandeling is niet gevonden...');
-        return res.redirect('/wandelingen');
-    }
-    res.render('wandelingen/edit', { wandeling });
-}));
-
-router.put('/:id', isIngelogd, isAuteur, validateWandeling, wrapAsync(async (req, res) => {
-    const { id } = req.params;
-    const wandeling = await Wandeling.findByIdAndUpdate(id, { ...req.body.wandeling });
-    req.flash('success', 'Wandeling gewijzigd!');
-    res.redirect(`/wandelingen/${wandeling._id}`)
-}));
-
-router.delete('/:id', isIngelogd, isAuteur, wrapAsync(async (req, res) => {
-    const { id } = req.params;
-    await Wandeling.findByIdAndDelete(id);
-    req.flash('success', 'Wandeling gewist!');
-    res.redirect('/wandelingen');
-}));
+router.delete('/:id', isIngelogd, isAuteur, wrapAsync(wandelingen.wisWandeling));
 
 module.exports = router;
