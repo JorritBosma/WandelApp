c03eb6a9008f2b7a23b50426fc5db30757452b38
JorritBosma <bosma.jorrit@gmail.com>
2020-12-11 11:44:56 +0100

    Added controllers for gebruikers and recensies, didn't need the video's.

--- routes/recensies.js
+++ routes/recensies.js
@@ -5,25 +5,10 @@ const Wandeling = require('../models/wandeling');
 const Recensie = require('../models/recensie');
 const wrapAsync = require('../helpers/wrapAsync');
 const ExpressError = require('../helpers/ExpressError');
+const recensies = require('../controllers/recensieController');
 
-router.post('/', isIngelogd, validateRecensie, wrapAsync(async (req, res) => {
-    const { id } = req.params;
-    const wandeling = await Wandeling.findById(id);
-    const recensie = new Recensie(req.body.recensie);
-    recensie.auteur = req.user._id;
-    wandeling.recensies.push(recensie);
-    await recensie.save();
-    await wandeling.save();
-    req.flash('success', 'Recensie toegevoegd!');
-    res.redirect(`/wandelingen/${wandeling._id}`)
-}));
+router.post('/', isIngelogd, validateRecensie, wrapAsync(recensies.maakRecensie));
 
-router.delete('/:recensieId', isIngelogd, isAuteurRecensie, wrapAsync(async (req, res) => {
-    const { id, recensieId } = req.params;
-    await Wandeling.findByIdAndUpdate(id, { $pull: { recensies: recensieId } })
-    await Recensie.findByIdAndDelete(recensieId);
-    req.flash('success', 'Recensie gewist!');
-    res.redirect(`/wandelingen/${id}`)
-}));
+router.delete('/:recensieId', isIngelogd, isAuteurRecensie, wrapAsync(recensies.wisRecensie));
 
 module.exports = router;
